20ae8f73cca0f6d7f8684b73522a71cfabddbcba
Astronoot <jorenlangbid2223@gmail.com>
2022-11-25 12:30:52 +0800

    - navigation.js code cleaning - RA4670 content update

--- navigations.js
+++ navigations.js
@@ -11,30 +11,31 @@
 
 $(document).ready(function() {
 
-    // Initialize components
+    // hide components
 
-    $("#image-preview-modal").hide();
+    var imagePreviewer = $("#image-preview-modal");
+    var tabItems = $(".tab-item");
+    var structuredTabs = $(".structured-content");
+    var contents = $(".structured-content .content-text");
+    var modalContents = $(".modal-content")
 
-    $(".tab-content").hide();
-    $(".tab-content").first().show();
-    
-    $(".structured-content .content-text").hide();
+    imagePreviewer.hide();
+    tabItems.hide();
+    tabItems.first().show();
+    contents.hide();
+    modalContents.slideToggle(1000);
 
     // only shows the first content-text of a structured-content
-    var structuredContents = $(".structured-content");
-    for (var i = 0; i < structuredContents.length; i++)
-        structuredContents.eq(i).children(".content-text").first().css("display", "block");
+    for (var i = 0; i < structuredTabs.length; i++)
+        structuredTabs.eq(i).children(".content-text").first().css("display", "block");
 
-    $(".nav-tab").first().toggleClass("active");
-    $(".content-nav .content-nav-tab:first-child").toggleClass("active");
-    $(".modal-content").slideToggle(1000);
-
-    // Event listeners
-
-    $(".modal-body h4").click(function() {
-        $(this).parent().children(".modal-content").slideToggle(1000);
-    });
-
+    // activate first tabs
+    var firstNavTab = $(".nav-tab").first();
+    firstNavTab.toggleClass("active");
+    var firstContentNavs = $(".content-nav .content-nav-tab:first-child");
+    firstContentNavs.toggleClass("active");
+    
+    // For Tab Items
     $(".nav-tab").click(function() {
 
         var navTabs = $(this).parent().children();
@@ -43,35 +44,47 @@ $(document).ready(function() {
         navTabs.removeClass("active");
         $(this).addClass("active");
 
-        $(".tab-content").hide();
-        $(".tab-content").eq(index).css("display", "grid");
+        tabItems.hide();
+        tabItems.eq(index).css("display", "grid");
 
     });
 
     $(".content-nav-tab").click(function() {
 
-        var structuredContents = $(this).parent().parent().children(".content-text");
-        var sections = $(this).parent().children();
-        var index = sections.index(this);
+        var contents = $(this).parent().parent().children(".content-text");
+        var contentNavs = $(this).parent().children();
+        var index = contentNavs.index(this);
 
-        sections.removeClass("active");
+        contentNavs.removeClass("active");
         $(this).addClass("active");
 
-        structuredContents.hide();
-        structuredContents.eq(index).css("display", "block");
+        contents.hide();
+        contents.eq(index).css("display", "block");
 
-    });
+    }); 
 
     $(".image-grid img").click(function() {
-        $("#image-preview-modal").fadeIn();
+
         var src = $(this).attr("src");
         var ref = $(this).attr("ref");
+
+        imagePreviewer.fadeIn();
         $("#image-preview-modal img").attr("src", src);
         $("#image-preview-modal p").text(ref);
+
     });
 
     $("#image-preview-modal img").click(function() {
-        $("#image-preview-modal").fadeOut();
+
+        imagePreviewer.fadeOut();
+
+    });
+
+    $(".modal-body h4").click(function() {
+
+        var modalContent = $(this).parent().children(".modal-content");
+        modalContent.slideToggle(1000);
+        
     });
 
 });
