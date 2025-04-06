8559101569acdc5918cbe01bf6d08290edb85805
Nootnoot <thepotatoboi>
2025-04-06 12:12:47 +0800

    try

--- public/Main.js
+++ public/Main.js
@@ -1,16 +1,16 @@
-import $ from "./Components/Libs/jQuery.js";
-import BoxedQuote from "./Components/BoxedQuote.js";
-import ContentImage from "./Components/ContentImage.js";
-import ImageGrid from "./Components/ImageGrid.js";
-import SlidedownModal from "./Components/SlidedownBox.js";
-import HeaderBox from "./Components/HeaderBox.js";
-import TitleBox from "./Components/TitleBox.js";
-import ArticleTabber from "./Components/ArticleTabber.js";
-import ArticleWithTabs from "./Components/ArticleWithTabs.js";
-import ReferenceList from "./Components/ReferenceList.js";
-import FooterBox from "./Components/FooterBox.js";
-import Metadata from "./Metadata.js";
-import SelectionSheet from "./Components/SelectionSheet.js";
+import $ from "/Components/Libs/jQuery.js";
+import BoxedQuote from "/Components/BoxedQuote.js";
+import ContentImage from "/Components/ContentImage.js";
+import ImageGrid from "/Components/ImageGrid.js";
+import SlidedownModal from "/Components/SlidedownBox.js";
+import HeaderBox from "/Components/HeaderBox.js";
+import TitleBox from "/Components/TitleBox.js";
+import ArticleTabber from "/Components/ArticleTabber.js";
+import ArticleWithTabs from "/Components/ArticleWithTabs.js";
+import ReferenceList from "/Components/ReferenceList.js";
+import FooterBox from "/Components/FooterBox.js";
+import Metadata from "/Metadata.js";
+import SelectionSheet from "/Components/SelectionSheet.js";
 
 class Main {
 
