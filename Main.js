b0aedb9c4dcf696d95042ea47de14b370aad3ec3
noobnoot <jorenlangbid2223@gmail.com>
2026-06-14 21:34:22 +0800

    added ACSCI banner, Math 9 peta

--- Main.js
+++ Main.js
@@ -2,7 +2,7 @@ import Server from "./Server.js";
 
 class Entry {
     static main() {
-        const server = new Server("0.0.0.0", 8080);
+        const server = new Server("127.0.0.1", 8080);
         server.start();
     }
 }
